0e7aef63b3a04b2776f8e1c4e960901157bd4c10
Lasse Huber-Saffer <lasse@huber-saffer.de>
2026-02-28 18:04:12 +0100

    Added CactusData constructors and comments

--- Octogarden/Assets/Scripts/Cactus/CactusData.cs
+++ Octogarden/Assets/Scripts/Cactus/CactusData.cs
@@ -1,8 +1,34 @@
 public class CactusData
 {
+    // Flavor text name
     public string Name { get; set; }
+
+    // Combinatorial traits
     public CactusClass Class { get; set; }
     public CactusOutfitType OutfitType { get; set; }
     public CactusFlowerColor FlowerColor { get; set; }
     public CactusPotType PotType { get; set; }
+    public CactusAffix Affixes { get; set; }
+
+    // TODO: Stat system
+
+    public CactusData(string name, CactusClass mechanicalClass)
+    {
+        Name = name;
+        Class = mechanicalClass;
+        OutfitType = CactusOutfitType.Basic;
+        FlowerColor = CactusFlowerColor.Red;
+        PotType = CactusPotType.Ceramic;
+        Affixes = CactusAffix.None;
+    }
+
+    public CactusData(CactusData other)
+    {
+        Name = (string)other.Name.Clone();
+        Class = other.Class;
+        OutfitType = other.OutfitType;
+        FlowerColor = other.FlowerColor;
+        PotType = other.PotType;
+        Affixes = other.Affixes;
+    }
 }
